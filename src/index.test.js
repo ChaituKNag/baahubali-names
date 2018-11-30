2885ff9dddd16270d4a356932bb50514a2510a2c
Naga Chaitanya Konada <nkonada@deloitte.com>
2018-11-30 20:15:05 +0530

    wrote good tests

--- src/index.test.js
+++ src/index.test.js
@@ -2,7 +2,23 @@ var { expect } = require('chai');
 var baahubali = require('./index');
 
 describe('baahubali-names', () => {
-    it('should work', () => {
-        expect(true).to.be.true;
-    })
+    describe('all', () => {
+        it('should be an array of strings', () => {
+            expect(baahubali.all).to.satisfy(array => array.every(item => typeof item === 'string'));
+        });
+
+        it('should contain `Amarendra Baahubali (Baahu)`', () => {
+          expect(baahubali.all).to.include('Amarendra Baahubali (Baahu)');
+        })
+        
+    });
+
+    describe('random', () => {
+        it('should return a random item from the baahubali.all', () => {
+        var randomItem = baahubali.random();
+
+        expect(baahubali.all).to.include(randomItem);
+        })
+        
+    });
 })
