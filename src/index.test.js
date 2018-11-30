d82bd5ac2deca50b4ffce1292cc9297d1ea4514b
Naga Chaitanya Konada <nkonada@deloitte.com>
2018-11-30 20:03:08 +0530

    Added mocha and chai to my dev dependencies

---
+++ src/index.test.js
@@ -0,0 +1,8 @@
+var { expect } = require('chai');
+var baahubali = require('./index');
+
+describe('baahubali-names', () => {
+    it('should work', () => {
+        expect(true).to.be.true;
+    })
+})
